c2951e0f488b436bd1b45a4f38bd614049b47174
Yelena2022 <iva_eleno4ka@mail.ru>
2022-10-07 21:24:32 +0300

    edit 07.10.22

--- project_0/game_v23.py
+++ project_0/game_v23.py
@@ -1,20 +1,20 @@
 """Игра: 'Угадай число'""" #3
 #загадывает и угадывает машина. Решение с функцией
 
-import numpy as npnp
+import numpy as np
 
 def random_predict(number: int=np.random.randint(1, 101)) -> int:
     
     min = 1
-    max  =  100
+    max = 100
 
     predict_number = int(np.random.randint(min, max + 1)) #машина начинает отгадывать число
     count = 0  #подсчет количества попыток на угадывание числа
 
-    while != != number:
+    while predict_number != number:
         count += 1
     
-        if > > number:
+        if predict_number > number:
             max = predict_number
             predict_number = (min + max) // 2
         
@@ -35,7 +35,7 @@ def score_game(random_predict) -> int:
     for number in random_array:
         count_ls.append(random_predict(number))
     score = int(np.mean(count_ls))
-    print(f"Этот алгоритм угадывает число в среднем за: {score} попытки.")
+    print(f"Этот алгоритм угадывает загаданное число {number} в среднем за: {score} попытки.")
     return score
 
 score_game(random_predict)
